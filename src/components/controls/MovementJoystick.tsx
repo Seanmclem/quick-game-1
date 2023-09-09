c053c1bf04100520dcd6f8e6318940cbdc28f878
Seanmclem <seanmclements@gmail.com>
2023-09-08 21:42:35 -0400

    basic on-screen joystick working

---
+++ src/components/controls/MovementJoystick.tsx
@@ -0,0 +1,69 @@
+import { Joystick } from "react-joystick-component";
+import { useInputs, useInputsStore } from "../../stores/input-store-hooks";
+
+export const MovementJoystick = () => {
+  const update_movement = useInputsStore((state) => state.update_movement);
+
+  return (
+    <div style={{ position: "absolute", top: 200, zIndex: 10 }}>
+      <Joystick
+        size={100}
+        // sticky={true}
+        baseColor="red"
+        stickColor="blue"
+        move={(e) => {
+          console.log(e);
+          const foo = {
+            type: "move",
+            x: 0.145234375,
+            y: -0.175625,
+            direction: "BACKWARD",
+            distance: 22.78972670012535,
+          };
+
+          const is_forward = ({ y }) => y > 0.2;
+          const is_backward = ({ y }) => y < -0.2;
+
+          const is_left = ({ x }) => x < -0.2;
+          const is_right = ({ x }) => x > 0.2;
+
+          const is_run = ({ distance }) => distance > 75;
+
+          const forward = is_forward(e);
+          const backward = is_backward(e);
+          const leftward = is_left(e);
+          const rightward = is_right(e);
+          const run = is_run(e);
+
+          const any_pressed =
+            forward || backward || leftward || rightward || run;
+
+          // prevents dude sinking and stuff when moving in dead-zone
+          if (any_pressed) {
+            update_movement({
+              forward: is_forward(e),
+              backward: is_backward(e),
+              leftward: is_left(e),
+              rightward: is_right(e),
+              run: is_run(e),
+            });
+          }
+
+          // greater than -0.20 and less than 0.20
+          // const is_deadZone = (num) => num > -0.2 && num < 0.2;
+        }}
+        stop={() =>
+          update_movement({
+            forward: false,
+            backward: false,
+            leftward: false,
+            rightward: false,
+            run: false,
+          })
+        }
+
+        // pos={{ x: 100, y: 100 }}
+      ></Joystick>
+    </div>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
 import { Leva } from "leva";
-import { Joystick } from "react-joystick-component";
 import { OptionsButton } from "./components/ui/OptionsButton";
+import { MovementJoystick } from "./components/controls/MovementJoystick";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
@@ -12,39 +12,8 @@ root.render(
   <>
     <Leva collapsed />
     {/* Canvas z-index is basically 0, so 1+ will overlay it */}
-    <div style={{ position: "absolute", top: 200, zIndex: 10 }}>
-      <Joystick
-        size={100}
-        // sticky={true}
-        baseColor="red"
-        stickColor="blue"
-        move={(e) => {
-          console.log(e);
-          const foo = {
-            type: "move",
-            x: 0.145234375,
-            y: -0.175625,
-            direction: "BACKWARD",
-            distance: 22.78972670012535,
-          };
 
-          const is_forward = ({ y }) => y > 0.2;
-          const is_backward = ({ y }) => y < -0.2;
-
-          const is_left = ({ x }) => x < -0.2;
-          const is_right = ({ x }) => x > 0.2;
-
-          const is_run = ({ distance }) => distance > 75;
-
-          // greater than -0.20 and less than 0.20
-          // const is_deadZone = (num) => num > -0.2 && num < 0.2;
-        }}
-        stop={() => null}
-
-        // pos={{ x: 100, y: 100 }}
-      ></Joystick>
-    </div>
-
+    <MovementJoystick />
     <OptionsButton />
 
     <Canvas
